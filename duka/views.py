a091e2ffd1b53e6280a7ba6b413d5fcf0dc4b42c
SEJJOH-JOSEH <37900292+SEJJOH-JOSEH@users.noreply.github.com>
2020-09-16 17:22:24 +0300

    initial commit
    displaying quantity and total price in the checkout.

---
+++ duka/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from .models import *
+
+
+# Create your views here.
+def duka(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, "duka/duka.html", context)
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
+    return render(request, "duka/checkout.html", context)
+
+
+def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
+    return render(request, "duka/cart.html", context)
